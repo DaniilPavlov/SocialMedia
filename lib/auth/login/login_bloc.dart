72f2fd70fcd946c5ec11a0db6d8bbf6f29e91ee4
DaniilPavlov <cool.sharagin@mail.ru>
2021-08-16 12:50:01 +0300

    Login Page with validation

---
+++ lib/auth/login/login_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:social_media/auth/form_submission_status.dart';
+
+import '../auth_repository.dart';
+
+part 'login_event.dart';
+
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepo;
+
+  LoginBloc({required this.authRepo}) : super(LoginState());
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    // Username updated
+    if (event is LoginUsernameChanged) {
+      yield state.copyWith(username: event.username);
+
+      // Password updated
+    } else if (event is LoginPasswordChanged) {
+      yield state.copyWith(password: event.password);
+
+      // Form submitted
+    } else if (event is LoginSubmitted) {
+      yield state.copyWith(formStatus: FormSubmitting());
+
+      try {
+        await authRepo.login();
+        yield state.copyWith(formStatus: SubmissionSuccess());
+      } catch (e) {
+        yield state.copyWith(formStatus: SubmissionFailed(e as Exception));
+      }
+    }
+  }
+}
